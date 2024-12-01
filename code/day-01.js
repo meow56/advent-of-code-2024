baee8ec73f8132ba4e396089d8d40a7872976cf3
meow56 <spinattacktotheface@gmail.com>
2024-11-30 23:11:23 -0600

    Day 01.2
    Good thing I can actually read this time.

--- code/day-01.js
+++ code/day-01.js
@@ -10,6 +10,9 @@ function day01(input) {
 		list2.push(+(entry[2]));
 	}
 
+	let unsorted1 = list1.slice();
+	let unsorted2 = list2.slice();
+
 	list1.sort((a, b) => b - a);
 	list2.sort((a, b) => b - a);
 
@@ -18,5 +21,23 @@ function day01(input) {
 		totalDist += Math.abs(list1[i] - list2[i]);
 	}
 
+	let counts = new Map();
+
+	for(let i = 0; i < unsorted2.length; i++) {
+		if(counts.has(unsorted2[i])) {
+			counts.set(unsorted2[i], counts.get(unsorted2[i]) + 1);
+		} else {
+			counts.set(unsorted2[i], 1);
+		}
+	}
+
+	let simScore = 0;
+	for(let i = 0; i < unsorted1.length; i++) {
+		if(counts.has(unsorted1[i])) {
+			simScore += counts.get(unsorted1[i]) * unsorted1[i];
+		}
+	}
+
 	displayCaption(`The total distance is ${totalDist}.`);
+	displayCaption(`The similarity score is ${simScore}.`);
 }
