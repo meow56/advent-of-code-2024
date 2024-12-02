8ee2104d31368309c360db6f03653638c0940dbc
meow56 <spinattacktotheface@gmail.com>
2024-12-01 23:10:44 -0600

    Day 02.1
    Actual server problems on AoC side, impossible.

--- code/day-02.js
+++ code/day-02.js
@@ -1,9 +1,25 @@
 "use strict";
 
 function day02(input) {
-	const FILE_REGEX = /./g;
+	const FILE_REGEX = /.+/g;
 	let entry;
+	let reports = [];
 	while(entry = FILE_REGEX.exec(input)) {
-
+		let levels = entry[0].split(' ').map(e => +e);
+		reports.push(levels);
 	}
+
+	let totalSafe = reports.reduce(function(acc, val) {
+		let isUp = val[1] - val[0] > 0;
+		for(let i = 1; i < val.length; i++) {
+			let diff = val[i] - val[i - 1];
+			if((diff > 0) !== isUp) {
+				return acc;
+			}
+			if(Math.abs(diff) > 3 || diff === 0) return acc;
+		}
+		return acc + 1;
+	}, 0);
+
+	displayCaption(`The total safe reports is ${totalSafe}.`);
 }
