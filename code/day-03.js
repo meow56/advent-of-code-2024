282ffe5ec245079023fc402bd2f2854e0a9cb6af
meow56 <spinattacktotheface@gmail.com>
2024-12-02 23:09:08 -0600

    Day 03.2
    Boy, I'm glad I know how to use regex (x2).

--- code/day-03.js
+++ code/day-03.js
@@ -1,12 +1,21 @@
 "use strict";
 
 function day03(input) {
-	const FILE_REGEX = /mul\((\d+),(\d+)\)/g;
+	const FILE_REGEX = /(?:mul\((\d+),(\d+)\))|do\(\)|don't\(\)/g;
 	let entry;
-	let sum = 0;
+	let sum1 = 0;
+	let sum2 = 0;
+	let enabled = true;
 	while(entry = FILE_REGEX.exec(input)) {
-		sum += +entry[1] * +entry[2];
+		if(entry[0].startsWith('mul')) {
+			let product = +entry[1] * +entry[2];
+			sum1 += product;
+			if(enabled) sum2 += product;
+		} else {
+			enabled = !entry[0].startsWith("don't");
+		}
 	}
 
-	displayCaption(`The sum is ${sum}.`);
+	displayCaption(`The sum is ${sum1}.`);
+	displayCaption(`The sum 2 is ${sum2}.`);
 }
