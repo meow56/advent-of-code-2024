b35f22ff12664c0896de1a9bcf0268f72efd12e5
meow56 <spinattacktotheface@gmail.com>
2024-12-02 23:08:23 -0600

    Day 03.1
    Boy, I'm glad I know how to use regex.

--- code/day-03.js
+++ code/day-03.js
@@ -1,9 +1,12 @@
 "use strict";
 
 function day03(input) {
-	const FILE_REGEX = /./g;
+	const FILE_REGEX = /mul\((\d+),(\d+)\)/g;
 	let entry;
+	let sum = 0;
 	while(entry = FILE_REGEX.exec(input)) {
-
+		sum += +entry[1] * +entry[2];
 	}
+
+	displayCaption(`The sum is ${sum}.`);
 }
