943274270a67aa7bca1033283a342d417b1c14fe
meow56 <spinattacktotheface@gmail.com>
2024-12-03 00:32:04 -0600

    Day 03.3
    Boy, I'm sad that I don't know how to use regex. (I tried to use regex to do the job of replaceMul...)

--- code/day-03.js
+++ code/day-03.js
@@ -16,6 +16,28 @@ function day03(input) {
 		}
 	}
 
+	function replaceMul(match, offset, string, groups) {
+		while(offset >= 0) {
+			offset--;
+			if(string.slice(offset).startsWith(`do()`)) {
+				return `<span class='skyblue'>${match}</span>`;
+			} else if(string.slice(offset).startsWith(`don't()`)) {
+				return `<span class='lightblue'>${match}</span>`;
+			}
+		}
+		return `<span class='skyblue'>${match}</span>`;
+	}
+
+	let newInput = input.replaceAll('<', '&lt;')
+						.replaceAll('>', '&gt;')
+						.replace(/mul\(\d+,\d+\)/g, replaceMul)
+						.replace(/do\(\)/g, `<span class='green'>$&</span>`)
+						.replace(/don't\(\)/g, `<span class='red'>$&</span>`);
+	displayText(newInput);
+
 	displayCaption(`The sum is ${sum1}.`);
-	displayCaption(`The sum 2 is ${sum2}.`);
+	displayCaption(`The sum with do() and don't() is ${sum2}.`);
+	displayCaption(`The "code" is shown.`);
+	displayCaption(`Valid mul() calls are shown highlighted blue. The lighter shade of blue is for mul() calls that are disabled by don't().`);
+	displayCaption(`do() calls are highlighted in green, while don't() calls are highlighted in red.`);
 }
