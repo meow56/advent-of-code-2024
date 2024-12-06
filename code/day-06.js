0b189b7cbf5d6c6bb1f6588ae6a93a31d3f21487
meow56 <spinattacktotheface@gmail.com>
2024-12-05 23:22:25 -0600

    Day 06.2
    Yay. I'm glad the bug was as simple as switching i and j in the function call.

--- code/day-06.js
+++ code/day-06.js
@@ -11,6 +11,7 @@ function day06(input) {
 			guard = [entry[0].indexOf('^'), grid.length - 1, [0, -1]];
 		}
 	}
+	let guardCopy = guard.slice();
 
 	let visited = new Map();
 	while(true) {
@@ -33,5 +34,40 @@ function day06(input) {
 		}
 	}
 
+	function poopyLoopy(tile) {
+		let visited = new Map();
+		let guard = guardCopy.slice();
+		while(true) {
+			let mapKey = guard.join(',');
+			if(visited.has(mapKey)) return true;
+			visited.set(mapKey);
+			let nextPos = [guard[0] + guard[2][0], guard[1] + guard[2][1]];
+			let nextLocation = grid[nextPos[1]]?.[nextPos[0]];
+			if(nextLocation === undefined) return false;
+			if(nextLocation === '#' || (nextPos[0] === tile[0] && nextPos[1] === tile[1])) {
+				// 0, -1 -> 1, 0
+				// 1, 0 -> 0, 1
+				// 0, 1 -> -1, 0
+				// -1, 0 -> 0, -1
+				guard[2] = [-guard[2][1], guard[2][0]];
+			} else {
+				guard[0] = nextPos[0];
+				guard[1] = nextPos[1];
+			}
+		}
+	}
+
+	let loopers = 0;
+	for(let i = 0; i < grid.length; i++) {
+		for(let j = 0; j < grid[i].length; j++) {
+			if(grid[i][j] === '#') continue;
+			if(grid[i][j] === '^') continue;
+			let mapKey = `${j},${i}`;
+			if(!visited.has(mapKey)) continue;
+			if(poopyLoopy([j, i])) loopers++;
+		}
+	}
+
 	displayCaption(`The total number of visited tiles is ${visited.size}.`);
+	displayCaption(`The number of possible loops is ${loopers}.`);
 }
