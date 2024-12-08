39229d6c52d1dac3acdcc18f7ee994ddbb437cf8
meow56 <spinattacktotheface@gmail.com>
2024-12-07 23:28:39 -0600

    Day 08.3
    Simple visualization this time. Nothing much to think about. Here's some food for thought, though: why the heck did I make antinode and antinode2 maps instead of sets?

--- code/day-08.js
+++ code/day-08.js
@@ -6,8 +6,10 @@ function day08(input) {
 	let antennae = new Map();
 	let row = 0;
 	let colSize;
+	let grid = [];
 	while(entry = FILE_REGEX.exec(input)) {
 		let line = entry[0].split("");
+		grid.push(line);
 		colSize = line.length;
 		for(let col = 0; col < line.length; col++) {
 			if(line[col] === ".") continue;
@@ -54,4 +56,38 @@ function day08(input) {
 
 	displayCaption(`The number of unique antinodes is ${antinodes.size}.`);
 	displayCaption(`The number of resonant antinodes is ${antinodes2.size}.`);
+	displayCaption(`The grid is shown.`);
+	displayCaption(`The darker shade of tiles (▓) are for part 1. Some of them are obscured by antennae.`);
+	displayCaption(`Lightly shaded tiles (▒) are for part 2. Just remember that part 1 tiles and all antennae also count.`);
+
+	let newGrid = grid.map(e => e.slice());
+	for(let antinode of antinodes2.keys()) {
+		let coord = antinode.split(",").map(e => +e);
+		newGrid[coord[0]][coord[1]] = "▒";
+	}
+
+	for(let antinode of antinodes.keys()) {
+		let coord = antinode.split(",").map(e => +e);
+		newGrid[coord[0]][coord[1]] = "▓";
+	}
+
+	for(let row = 0; row < grid.length; row++) {
+		for(let col = 0; col < grid[row].length; col++) {
+			if(grid[row][col] !== ".") {
+				newGrid[row][col] = grid[row][col];
+			}
+		}
+	}
+
+	for(let row = 0; row < newGrid.length; row++) {
+		for(let col = 0; col < newGrid[row].length; col++) {
+			if(newGrid[row][col] === ".") {
+				newGrid[row][col] = " ";
+			}
+		}
+	}
+
+	for(let row of newGrid) {
+		displayText(row.join(""));
+	}
 }
