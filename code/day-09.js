bb622f4fa59bd8be5c1b70914bd987327fccd29c
meow56 <spinattacktotheface@gmail.com>
2024-12-08 23:21:20 -0600

    Day 09.1
    I was gonna do it complicatedly but I realized this is better. Also visualization jumpscare.

--- code/day-09.js
+++ code/day-09.js
@@ -3,7 +3,44 @@
 function day09(input) {
 	const FILE_REGEX = /./g;
 	let entry;
+	let files = [];
+	let freeSpace = [];
+	let isFile = true;
 	while(entry = FILE_REGEX.exec(input)) {
-
+		if(isFile) {
+			files.push(+(entry[0]));
+		} else {
+			freeSpace.push(+(entry[0]));
+		}
+		isFile = !isFile;
 	}
+
+	files = files.flatMap((num, index) => (new Array(num)).fill(index));
+	let checksum = 0;
+	let backIndex = files.length - 1;
+	let lastNum = 0;
+	let freeIndex = 0;
+	let currentPos = 0;
+	let display = '';
+	for(let frontIndex = 0; frontIndex < backIndex; frontIndex++) {
+		if(files[frontIndex] !== lastNum) {
+			// Fill in empties first.
+			let empties = freeSpace[freeIndex++];
+			for(let i = 0; i < empties; i++) {
+				checksum += currentPos * files[backIndex];
+				display += files[backIndex];
+				currentPos++;
+				backIndex--;
+				if(frontIndex > backIndex) break;
+			}
+			if(frontIndex > backIndex) break;
+			lastNum = files[frontIndex];
+		}
+		display += files[frontIndex];
+		checksum += currentPos * files[frontIndex];
+		currentPos++;
+	}
+
+	displayCaption(`The checksum is ${checksum}.`);
+	displayText(display);
 }
