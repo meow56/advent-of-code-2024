3258112d85bff29239630b8f13949b915268ced8
meow56 <spinattacktotheface@gmail.com>
2024-12-13 23:10:59 -0600

    Day 14.1
    When you type robot[4] instead of robot[3] like a dum-dum.

--- code/day-14.js
+++ code/day-14.js
@@ -1,9 +1,20 @@
 "use strict";
 
 function day14(input) {
-	const FILE_REGEX = /./g;
+	const FILE_REGEX = /p=(\d+),(\d+) v=(-?\d+),(-?\d+)/g;
 	let entry;
+	let robots = [];
 	while(entry = FILE_REGEX.exec(input)) {
-
+		robots.push([+entry[1], +entry[2], +entry[3], +entry[4]]);
 	}
+
+	let quads = [0, 0, 0, 0];
+	for(let robot of robots) {
+		let finalX = (((robot[0] + (100 * robot[2])) % 101) + 101) % 101;
+		let finalY = (((robot[1] + (100 * robot[3])) % 103) + 103) % 103;
+		if(finalX === 50 || finalY === 51) continue;
+		quads[(finalX > 50 ? 2 : 0) + (finalY > 51 ? 1 : 0)]++;
+	}
+
+	displayCaption(`The safety score is ${quads.reduce((a, v) => a * v)}.`);
 }
