ae7a0e8c55f383150635963da29f5fdfc1456886
meow56 <spinattacktotheface@gmail.com>
2024-12-21 23:23:24 -0600

    Day 22.2
    Welp. Brute force baybeeeeee

--- code/day-22.js
+++ code/day-22.js
@@ -21,12 +21,35 @@ function day22(input) {
 	}
 
 	let sum = 0n;
+	let diffMap = new Map();
 	for(let num of numbers) {
+		let thisDiffMap = new Map();
+		let last4Diff = [];
+		let prevNum = num % 10n;
 		for(let i = 0; i < 2000; i++) {
 			num = generate(num);
+			last4Diff.push(num % 10n - prevNum);
+			if(last4Diff.length === 4) {
+				let mapKey = last4Diff.join(",");
+				if(!thisDiffMap.has(mapKey)) {
+					thisDiffMap.set(mapKey, num % 10n);
+				}
+				last4Diff.shift();
+			}
+			prevNum = num % 10n;
 		}
 		sum += num;
+
+		for(let [key, value] of thisDiffMap.entries()) {
+			diffMap.set(key, (diffMap.get(key) ?? 0n) + value);
+		}
+	}
+
+	let maxBananas = 0;
+	for(let val of diffMap.values()) {
+		if(val > maxBananas) maxBananas = val;
 	}
 
 	displayCaption(`The sum of secret numbers is ${sum}.`);
+	displayCaption(`The most number of bananas you can get is ${maxBananas}.`);
 }
