c6148d1286a48f74125f2a7bf9be8808810fcbe1
meow56 <spinattacktotheface@gmail.com>
2024-12-21 23:11:11 -0600

    Day 22.1
    Busting out the BigInt for this one. Too lazy to figure out how to make regular ints not signed.

--- code/day-22.js
+++ code/day-22.js
@@ -1,9 +1,32 @@
 "use strict";
 
 function day22(input) {
-	const FILE_REGEX = /./g;
+	const FILE_REGEX = /(\d+)/g;
 	let entry;
+	let numbers = [];
 	while(entry = FILE_REGEX.exec(input)) {
-
+		numbers.push(BigInt(+entry[0]));
 	}
+
+	function mixPrune(val, num) {
+		let xor = val ^ num;
+		return xor % 16777216n;
+	}
+
+	function generate(num) {
+		num = mixPrune(num << 6n, num);
+		num = mixPrune(num >> 5n, num);
+		num = mixPrune(num << 11n, num);
+		return num;
+	}
+
+	let sum = 0n;
+	for(let num of numbers) {
+		for(let i = 0; i < 2000; i++) {
+			num = generate(num);
+		}
+		sum += num;
+	}
+
+	displayCaption(`The sum of secret numbers is ${sum}.`);
 }
