56f78271b49e37af63d1d501fb1e88b6e5827e79
meow56 <spinattacktotheface@gmail.com>
2024-12-24 13:29:47 -0600

    Day 24.3
    Hey, I did do it after all. Nice. Glad that the categorization idea worked out.

--- code/day-24.js
+++ code/day-24.js
@@ -60,25 +60,59 @@ function day24(input) {
 	 * ^ (this (carry) means the result of (bit adder) AND carry)
 	 * The carry in of an adder is the "true carry" of the previous.
 	 */
-	let outputs = [];
-	let carrys = [];
-	let overflow = [];
-	let bitAdders = [[]];
-	let trueCarrys = [];
+	let lOutputs = [];
+	let lCarrys = [];
+	let lOverflow = [];
+	let lBitAdders = [];
+	let lTrueCarrys = [];
+
+	let rOutputs = [];
+	let rOverflowsAndCarrys = [];
+	let rBitAddersAndTrueCarrys = [];
 	for(let gate of startGates) {
-		if(gate[0].startsWith("z")) {
-			outputs[parseInt(gate[0].slice(1), 10)] = gate.slice();
-		} else if(gate[1].startsWith("x") || gate[3].startsWith("x")) {
+		if(gate[1].startsWith("x") || gate[3].startsWith("x")) {
 			if(gate[2] === "AND") {
-				overflow[parseInt(gate[1].slice(1), 10)] = gate.slice();
+				if(gate[1].endsWith("00")) {
+					lTrueCarrys.push(gate.slice());
+				} else {
+					lOverflow.push(gate.slice());
+				}
 			} else {
-				bitAdders[parseInt(gate[1].slice(1), 10)] = gate.slice();
+				if(gate[1].endsWith("00")) {
+					lOutputs.push(gate.slice());
+				} else {
+					lBitAdders.push(gate.slice());
+				}
 			}
 		} else {
 			if(gate[2] === "AND") {
-				carrys.push(gate.slice());
+				lCarrys.push(gate.slice());
 			} else if(gate[2] === "OR") {
-				trueCarrys.push(gate.slice());
+				lTrueCarrys.push(gate.slice());
+			} else {
+				lOutputs.push(gate.slice());
+			}
+		}
+
+		if(gate[0].startsWith("z")) {
+			if(gate[0].endsWith("45")) {
+				rBitAddersAndTrueCarrys.push(gate.slice());
+			} else {
+				rOutputs.push(gate.slice());
+			}
+		} else {
+			let usage = [];
+			for(let gate2 of startGates) {
+				if(gate2[1] === gate[0] || gate2[3] === gate[0]) {
+					usage.push(gate2[2]);
+				}
+			}
+			if(usage.length === 1) {
+				rOverflowsAndCarrys.push(gate.slice());
+			} else if(usage.length === 2) {
+				rBitAddersAndTrueCarrys.push(gate.slice());
+			} else {
+				throw `Too many usages for ${gate}: ${usage.join(";")}`;
 			}
 		}
 	}
@@ -89,98 +123,50 @@ function day24(input) {
 		}
 	}
 
-	for(let gate of outputs) {
-		let in1 = gate[1];
-		let in2 = gate[3];
-		if(!exists(in1, bitAdders) && !exists(in2, bitAdders)) {
-			displayText(`${gate[0]} doesn't have a bit adder, weird.`);
-		} else if(exists(in1, bitAdders)) {
-			let result = exists(in1, bitAdders);
-			if(parseInt(result[1].slice(1), 10) !== parseInt(gate[0].slice(1), 10)) {
-				displayText(`${gate[0]} has the wrong bit adder.`);
-			}
-		} else {
-			let result = exists(in2, bitAdders);
-			if(parseInt(result[1].slice(1), 10) !== parseInt(gate[0].slice(1), 10)) {
-				displayText(`${gate[0]} has the wrong bit adder.`);
-			}
+	function stringGate(gate) {
+		return `${gate[1]} ${gate[2]} ${gate[3]} -> ${gate[0]}`;
+	}
+
+	let erroneous = [];
+
+	for(let gate of lOutputs) {
+		if(!exists(gate[0], rOutputs)) {
+			erroneous.push(gate[0]);
+			displayText(`TypeError: Expected right side of ${stringGate(gate)} to be output, but it isn't.`);
 		}
 	}
 
-	// kth <-> z12?
-	// gsd <-> z26?
-	// vpm <-> qnf?
-	// tbt <-> z32?
-	let kth = exists("kth", startGates);
-	let z12 = exists("z12", startGates);
-	let gsd = exists("gsd", startGates);
-	let z26 = exists("z26", startGates);
-	let vpm = exists("vpm", startGates);
-	let qnf = exists("qnf", startGates);
-	kth[0] = "z12";
-	z12[0] = "kth";
-	gsd[0] = "z26";
-	z26[0] = "gsd";
-	vpm[0] = "qnf";
-	qnf[0] = "vpm";
-
-
-	function quoteAddUnquote(x, y) {
-		let trueInputs = new Map();
-		let xBin = decToBin(x).toReversed();
-		let yBin = decToBin(y).toReversed();
-		for(let i = 0; i < 45; i++) {
-			let padI = (i.toString()).padStart(2, "0");
-			trueInputs.set(`x${padI}`, xBin[i] ?? 0);
-			trueInputs.set(`y${padI}`, yBin[i] ?? 0);
+	for(let gate of lCarrys) {
+		if(!exists(gate[0], rOverflowsAndCarrys)) {
+			erroneous.push(gate[0]);
+			displayText(`TypeError: Expected right side of ${stringGate(gate)} to be carry, but it isn't.`);
 		}
-		let gates = startGates.slice();
-		while(gates.length !== 0) {
-			let next = gates.shift();
-			if(trueInputs.has(next[1]) && trueInputs.has(next[3])) {
-				if(next[2] === "AND") {
-					trueInputs.set(next[0], trueInputs.get(next[1]) && trueInputs.get(next[3]));
-				} else if(next[2] === "OR") {
-					trueInputs.set(next[0], trueInputs.get(next[1]) || trueInputs.get(next[3]));
-				} else {
-					trueInputs.set(next[0], (trueInputs.get(next[1]) || trueInputs.get(next[3])) && !(trueInputs.get(next[1]) && trueInputs.get(next[3])));
-				}
-			} else {
-				gates.push(next);
-			}
-		}
-
-		let z = [];
-		for(let [key, value] of trueInputs.entries()) {
-			if(key[0] === "z") {
-				z[parseInt(key.slice(1), 10)] = value ? "1" : "0";
-			}
-		}
-
-		return z;
 	}
 
-	function decToBin(num) {
-		let start = 0;
-		while(2 ** (start + 1) < num) {
-			start++;
+	for(let gate of lOverflow) {
+		if(!exists(gate[0], rOverflowsAndCarrys)) {
+			erroneous.push(gate[0]);
+			displayText(`TypeError: Expected right side of ${stringGate(gate)} to be overflow, but it isn't.`);
 		}
-
-		let result = [];
-		for(; start >= 0; start--) {
-			if(num >= 2 ** start) {
-				result.push(1);
-				num -= 2 ** start;
-			} else {
-				result.push(0);
-			}
-		}
-
-		return result;
 	}
 
-	console.log(quoteAddUnquote(125223423342, 252424023423).toReversed().join(""));
-	console.log(decToBin(125223423342 + 252424023423).join("").padStart(46, "0"));
+	for(let gate of lBitAdders) {
+		if(!exists(gate[0], rBitAddersAndTrueCarrys)) {
+			erroneous.push(gate[0]);
+			displayText(`TypeError: Expected right side of ${stringGate(gate)} to be bit adder, but it isn't.`);
+		}
+	}
+
+	for(let gate of lTrueCarrys) {
+		if(!exists(gate[0], rBitAddersAndTrueCarrys)) {
+			erroneous.push(gate[0]);
+			displayText(`TypeError: Expected right side of ${stringGate(gate)} to be true carry, but it isn't.`);
+		}
+	}
 
 	displayCaption(`The number is ${parseInt(z.toReversed().join(""), 2)}.`);
+	displayCaption(`The wrong gate outputs are "${erroneous.toSorted().join(",")}".`);
+	displayCaption(`A list of "TypeError"s are shown.`);
+	displayCaption(`Each one specifies a gate whose input and output are at odds with each other.`);
+	displayCaption(`It specifies what type it was expecting the output to be.`);
 }
